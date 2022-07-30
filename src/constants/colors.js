d19e6ed3088b4b5a9a787f75f2341899e03c7b72
joelcg <joseplois@protonmail.com>
2022-07-30 11:08:01 +0700

    first commit
    test 1

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+const colors = [
+  ['rgb(204, 0, 204)', 'rgb(153, 0, 255)']
+];
+
+module.exports = colors;
---
+++ src/constants/messages.js
@@ -0,0 +1,26 @@
+const messages = [
+  ['Good', 'Morning,','babe!'],
+  ['Yeah, I had','Zero plans'],
+  ['So'],
+  ['i decided', 'to nerd', 'around'],
+  ['And found', 'this funky', 'song!'],
+  ['so i stir', 'up some', 'ideas'],
+  ['did some', 'tinkering &', 'engineering'],
+  ['yea,', 'engineering', 'lol'],
+  ['and lil bit', 'of stealing..'],
+  ['arts.'],
+  ['but off of', 'open source', 'works'],
+  ['so its', 'really legal'],
+  ['i think.'],
+  ['and', 'created', 'this!'],
+  ['anyway'],
+  ['i hope you', 'had a good', 'sleep'],
+  ['and that youre', 'off to a', 'good day!'],
+  ['but', 'meanwhile,'],
+  ['enjoy', 'the', 'music!'],
+  ['and..'],
+  ['I love', 'you! xoxo'],
+  ['youve reached', 'the end of', 'this message']
+];
+
+module.exports = messages;
